5c5fdeea97fdb1b82def3ce87c8e35b282f3d394
John Feng <atjohnfeng@gmail.com>
2022-01-21 18:26:52 -0800

    Add debug to CrashDetector script

--- 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
+++ 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.tag == "Player")
+        {
+            Debug.Log("You bumped your head!");
+        }
     }
 }
