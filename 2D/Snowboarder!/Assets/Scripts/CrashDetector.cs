16dabf807535408a8f0b668f688f9f6364fd5aa2
John Feng <atjohnfeng@gmail.com>
2022-02-07 18:18:32 -0800

    update scripts

--- 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
+++ 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("You bumped your head!");
+        SceneManager.LoadScene(0);
     }
 }
