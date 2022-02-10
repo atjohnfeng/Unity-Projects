80b8acc8d1d066ff62d47ec229207f631e850bdd
John Feng <atjohnfeng@gmail.com>
2022-02-09 18:24:52 -0800

    Add crash and finish effects

--- 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
+++ 2D/Snowboarder!/Assets/Scripts/CrashDetector.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem finishEffect;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        finishEffect.Play();
         Invoke("ReloadScene", loadDelay);
     }
 
