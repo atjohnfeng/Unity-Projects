c55b6e2ad4b2405e3f2c263c94bb6b29b51e51e0
John Feng <atjohnfeng@gmail.com>
2022-03-07 18:29:18 -0800

    Add scriptable object for question

---
+++ 2D/Quizical/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [SerializeField] string question = "Enter new question text here";
+
+    public string getQuestion()
+    {
+        return question;
+    }
+}
