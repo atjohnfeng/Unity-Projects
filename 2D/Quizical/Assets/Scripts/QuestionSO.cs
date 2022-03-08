52b747cabf4a6a3e65707605c5d518df098f8f84
John Feng <atjohnfeng@gmail.com>
2022-03-07 18:31:09 -0800

    Adjust text area

--- 2D/Quizical/Assets/Scripts/QuestionSO.cs
+++ 2D/Quizical/Assets/Scripts/QuestionSO.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
+    [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text here";
 
     public string getQuestion()
