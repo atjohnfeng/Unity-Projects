f1ae9ca19bae90a25dbd17d2e8ffce97554904f2
John Feng <atjohnfeng@gmail.com>
2022-03-10 18:14:30 -0800

    Add additional public getters

--- 2D/Quizical/Assets/Scripts/QuestionSO.cs
+++ 2D/Quizical/Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,21 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int answerIndex = 0;
 
     public string getQuestion()
     {
         return question;
+    }
+
+    public int getAnswerIndex()
+    {
+        return answerIndex;
+    }
+
+    public string getAnswer(int index)
+    {
+        return answers[index];
     }
 }
