650fc5829000e31618744435533e359112155191
John Feng <atjohnfeng@gmail.com>
2022-03-14 18:13:55 -0700

    add answer button logic

--- 2D/Quizical/Assets/Scripts/Quiz.cs
+++ 2D/Quizical/Assets/Scripts/Quiz.cs
@@ -7,9 +7,16 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
 
-    void Start()
+    async void Start()
     {
         questionText.text = question.getQuestion();
+
+        for (int i = 0; i < 4; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.getAnswer(i);
+        }
     }
 }
